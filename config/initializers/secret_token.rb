74d3346633afe1af4e322e7db12d576f696c9c1e
Alex Zanetti de Lima <trustlix@gmail.com>
2011-02-04 19:51:55 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cbhq::Application.config.secret_token = '6e16ad317c9ed238e4032a15af891dc09006b515ab40a70698a7ed477cde7b7e4f5f25ff392b8e5a9f7ec7ddc704711a61cc3a2a74fdc172655d37f5251b0db6'
