3b00848819e33c3be711fefa8f3845d4be54ba78
Alex Zanetti de Lima <trustlix@gmail.com>
2011-02-11 19:19:54 -0200

    testing

--- app/controllers/gitpush_controller.rb
+++ app/controllers/gitpush_controller.rb
@@ -3,8 +3,8 @@ require 'json/add/rails'
 class GitpushController < ApplicationController
 
   def updaterally
-    puts JSON.parse params["payload"]
-    puts "Blah!"
+  #  puts JSON.parse params["payload"]
+  #  puts "Blah!"
 
     render :nothing => true, :status => 200
   end
