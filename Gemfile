da9c1c77b7a88da878031f67cbae50606c450f5f
Alex Zanetti de Lima <trustlix@gmail.com>
2011-02-17 13:36:38 -0200

    added yaml gem

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.0.3'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'rally_rest_api', '1.0.3'
 gem 'json'
+gem 'yaml'
 
 group :development do
   gem 'rspec-rails'
--- app/controllers/gitpush_controller.rb
+++ app/controllers/gitpush_controller.rb
@@ -1,11 +1,12 @@
 require 'json/add/rails'
 #require 'ruby-debug'
+require 'yaml'
 
 class GitpushController < ApplicationController
 
   def updaterally
     puts "Post content:\n"
-    p JSON.parse(params["payload"])
+    p JSON.parse(params["payload"]).to_yaml
 
     render :nothing => true, :status => 200
   end
