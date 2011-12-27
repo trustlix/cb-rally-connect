19c73009bf658128b7cf0405ab0b2cb700a0e6de
Alex Zanetti de Lima <trustlix@gmail.com>
2011-12-27 17:35:41 -0200

    Read workspace value from env

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -5,6 +5,7 @@ CONFIG = YAML.load_file( "#{RAILS_ROOT}/config/config.yml" )[RAILS_ENV]
 # give precedence to settings coming from env
 CONFIG['rally_username'] = ENV['RALLY_USERNAME'] if ENV['RALLY_USERNAME']
 CONFIG['rally_password'] = ENV['RALLY_PASSWORD'] if ENV['RALLY_PASSWORD']
+CONFIG['rally_workspace'] = ENV['RALLY_WORKSPACE'] if ENV['RALLY_WORKSPACE']
 
 # conn to rally with default values
 RALLY_CONNECTOR = RallyConnector.new()
